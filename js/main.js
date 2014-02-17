295f84a02c19749fbcb32c0f163fd7cc19b6687e
x-masashik <x-masashik@JPC20152408.jp.sony.com>
2014-02-17 12:22:31 +0900

    debug

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@ $(document).on("mobileinit", function(){
         });
     }
 
-    $("div#top").on("pageshow", function() {
+    $("div#top").live("pagecontainershow", function() {
         console.log("pageshow of div#top triggered");
         setTimeout(5000);
         loginCheck();
