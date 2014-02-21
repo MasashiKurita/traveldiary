bef03b78a3e6d2002336eb178f7253f9844ffa17
x-masashik <x-masashik@JPC20152408.jp.sony.com>
2014-02-21 11:57:13 +0900

    debug

--- js/main.js
+++ js/main.js
@@ -134,6 +134,7 @@ $(document).on("pageshow", "div#main", function(event) {
                         zoom: 3,
                         mapTypeId: google.maps.MapTypeId.ROADMAP
                 };
+                $("div#mapcanvas").css("height",$(document).height());
                 var map = new google.maps.Map(document.getElementById('mapcanvas'), mapOptions);
                 var bounds = new google.maps.LatLngBounds();
 
@@ -207,8 +208,6 @@ $(document).on("pageshow", "div#main", function(event) {
                     }]
                 });
                 footmark.setMap(map);
-
-                ("div#mapcanvas").css("height",$(document).height());
 
             });
 
